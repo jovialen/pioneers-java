c2b75c17e520a56ae1624ee7e3481cb0c239a3de
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2024-02-23 17:04:55 +0100

    feat: initial commit

---
+++ src/main/java/com/github/jovialen/pioneers/Pioneers.java
@@ -0,0 +1,9 @@
+package com.github.jovialen.pioneers;
+
+import org.tinylog.Logger;
+
+public class Pioneers {
+    public static void main(String[] args) {
+        Logger.info("Hello, world!");
+    }
+}
