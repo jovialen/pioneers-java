61fbb3b02672eb6f34082981ff017d36cf5fac8f
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2024-03-24 18:23:13 +0100

    fix: make node3d translation relative to parent

--- src/main/java/com/github/jovialen/motor/graph/scene/transform/Node3D.java
+++ src/main/java/com/github/jovialen/motor/graph/scene/transform/Node3D.java
@@ -9,6 +9,7 @@ public class Node3D extends SceneNode {
     public Vector3f position = new Vector3f(0);
     public Quaternionf rotation = new Quaternionf().identity();
     public Vector3f scale = new Vector3f(1);
+    public boolean relative = true;
 
     public Matrix4f transform = new Matrix4f().identity();
 
@@ -18,7 +19,14 @@ public class Node3D extends SceneNode {
 
     @Override
     public void process(double deltaTime) {
+        Node3D parent = getParent(Node3D.class);
+        if (parent != null && relative) {
+            transform.set(parent.transform);
+        } else {
+            transform.identity();
+        }
         transform.identity().translate(position).rotate(rotation).scale(scale);
+
         super.process(deltaTime);
     }
 }
